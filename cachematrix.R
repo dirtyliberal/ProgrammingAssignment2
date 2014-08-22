80fdd68ef023b39b68c9042f7fb8a3c8f4c3921f
dirtyliberal <marco.dupersoy@gmail.com>
2014-08-22 00:32:57 -0400

    completed assignment

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,22 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  y <<- null
+  y <<- solve(x) #solves a given invertible matrix and caches it
+  m <<- x
 
 }
 
 
 ## Write a short comment describing this function
-
+## y is the cached inverse of x from makecacheMatrix 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  if (m == x){ ##Checks if x has not changed
+    
+    if(!is.null(y)) {
+      message("getting cached data")
+      return(y)  }
+  }
+  return(solve (x))
 }
